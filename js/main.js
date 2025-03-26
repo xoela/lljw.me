04bc28e847605c7b857f4969b1309f2da2b25541
xoela <122069993+xoela@users.noreply.github.com>
2025-03-26 23:47:28 +0100

    version 1 w/ media query

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// const switchTab = document.querySelector('.pgp');
+
+// switchTab.addEventListener('click', function(){
+//     alert('Switching page to show PGP key!');
+// }); 
+
+function showDcZex() {
+    alert('@zexush | 1125787108372648039');
+} 
+
+function showDcInternal() {
+    alert('@filthyaddict | 719261043397754950');
+}
+
+function showDcKip() {
+    alert('@caligirls | 110693017054089307');
+}
+
+//shit code alert!
